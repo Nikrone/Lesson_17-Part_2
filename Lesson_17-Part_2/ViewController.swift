9052c23e90f7678b7597a93ecf98a4feb3e1ed6b
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-15 15:51:53 +0300

    add full Lesson 17 part 2

--- Lesson_17-Part_2/ViewController.swift
+++ Lesson_17-Part_2/ViewController.swift
@@ -10,19 +10,41 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
     
     @IBAction func cameraButtonPressed() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func galeryButtonPressed() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
         
+        present(imagePicker, animated: true, completion: nil)
     }
 
 }
 
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        imageView.image = image
+    }
+}
